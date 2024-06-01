2e110481be6afc2331f5aa480f8c704ac418cae9
LT <support@teran.tech>
2024-06-01 09:19:07 -0700

    Created two new implementations of database models.

---
+++ PassLock.EntityFramework/PasswordDatabaseModel.cs
@@ -0,0 +1,52 @@
+using System;
+using PassLock.Commands;
+using PassLock.Core;
+
+namespace PassLock.EntityFramework
+{
+   public class PasswordDatabaseModel : IDatabaseModel<Password>
+   {
+      public List<Password> GetAll()
+      {
+         using (var db = new PDatabaseContext())
+         {
+            return db.Passwords.ToList();
+         }
+      }
+
+      public Password GetById(int id)
+      {
+         using (var db = new PDatabaseContext())
+         {
+            return db.Passwords.Where(x => x.Id == id).FirstOrDefault() ?? new Password();
+         }
+      }
+
+      public void Insert(Password model)
+      {
+         using (var db = new PDatabaseContext())
+         {
+            db.Passwords.Add(model);
+            db.SaveChanges();
+         }
+      }
+
+      public void Remove(Password model)
+      {
+         using (var db = new PDatabaseContext())
+         {
+            db.Passwords.Remove(model);
+            db.SaveChanges();
+         }
+      }
+
+      public void Update(Password model)
+      {
+         using (var db = new PDatabaseContext())
+         {
+            db.Passwords.Update(model);
+            db.SaveChanges();
+         }
+      }
+   }
+}
