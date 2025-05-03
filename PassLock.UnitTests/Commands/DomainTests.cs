12d88ce11cdd9e85c8d12be383a8d62c30f718bf
LT <support@teran.tech>
2025-05-03 07:18:14 -0700

    Enhanced DomainTests by using the When/Given/Then paradigm as well as adding more tests.

--- PassLock.UnitTests/Commands/DomainTests.cs
+++ PassLock.UnitTests/Commands/DomainTests.cs
@@ -12,49 +12,68 @@ namespace PassLock.UnitTests.Commands
    {
       [Theory]
       [AutoMoq]
-      public async Task DomainAdd_Test01(
-         [Frozen] Mock<IDatabaseModel<Domain>> repo)
-      {
-         var domainAddCmd = new DomainAddCommand(repo.Object, "hotmail.com");
-
-         Assert.True(await domainAddCmd.Execute());
-      }
-
-      [Theory]
-      [AutoMoq]
-      public async Task DomainList_Test01(
+      public async Task GivenValidDomain_WhenAdded_ResponseIsTrue(
          [Frozen] Mock<IDatabaseModel<Domain>> repo)
       {
          // Arrange
-         repo.Setup(x => x.GetAllAsync()).Returns(Task.FromResult(new List<Domain>() { }));
+         var domainAddCmd = new DomainAddCommand(repo.Object, "hotmail.com");
          // Act
-         var domainListCmd = new DomainListCommand(repo.Object);
+         var response = await domainAddCmd.Execute();
          // Assert
-         var response = await domainListCmd.Execute();
          Assert.True(response);
       }
 
       [Theory]
       [AutoMoq]
-      public async Task DomainRemove_Test01(
+      public async Task GivenInvalidDomain_WhenAdded_ResponseIsFalse(
          [Frozen] Mock<IDatabaseModel<Domain>> repo)
       {
-         var domainRemoveCmd = new DomainRemoveCommand(repo.Object);
-         // If no domain descriptors passed in command should return false
-         Assert.False(await domainRemoveCmd.Execute());
+         // Arrange
+         var domainAddCmd = new DomainAddCommand(repo.Object, string.Empty);
+         // Act
+         var response = await domainAddCmd.Execute();
+         // Assert
+         Assert.False(response);
       }
 
       [Theory]
       [AutoMoq]
-      public async Task DomainRemove_Test02(
+      public async Task GivenDomain_WhenListed_ResponseIsTrue(
+         [Frozen] Mock<IDatabaseModel<Domain>> repo)
+      {
+         // Arrange
+         repo.Setup(x => x.GetAllAsync()).ReturnsAsync(new List<Domain>() { });
+         var domainListCmd = new DomainListCommand(repo.Object);
+         // Act
+         var response = await domainListCmd.Execute();
+         // Assert
+         Assert.True(response);
+      }
+
+      [Theory]
+      [AutoMoq]
+      public async Task GivenDomainRemoveCommand_WhenMissingArguments_ResponseIsFalse(
+         [Frozen] Mock<IDatabaseModel<Domain>> repo)
+      {
+         // Arrange
+         var domainRemoveCmd = new DomainRemoveCommand(repo.Object);
+         // Act
+         var response = await domainRemoveCmd.Execute();
+         // Assert
+         Assert.False(response);
+      }
+
+      [Theory]
+      [AutoMoq]
+      public async Task GivenDomainRemoveCommand_WhenExecuted_ResponseIsTrue(
          [Frozen] Mock<IDatabaseModel<Domain>> repo)
       {
          // Arange
-         repo.Setup(x => x.GetByIdAsync(It.IsAny<Domain>())).Returns(Task.FromResult(new Domain()));
-         // Act
+         repo.Setup(x => x.GetByIdAsync(It.IsAny<Domain>())).ReturnsAsync(new Domain());
          var domainRemoveCmd = new DomainRemoveCommand(repo.Object, 1);
-         // Assert
+         // Act
          var response = await domainRemoveCmd.Execute();
+         // Assert
          Assert.True(response);
       }
    }
